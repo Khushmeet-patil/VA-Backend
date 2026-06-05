ffcf03414fadde80a27207850758e7704dbbb4a8
Khushmeet-patil <khushmeetpatil@gmail.com>
2026-06-05 14:54:00 +0530

    initialize public store routes for vendors, products, and site configurations

--- src/store/routes/public/index.routes.js
+++ src/store/routes/public/index.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllBanners } = require("../../controllers/banner.controller");
+const { getActiveBanners } = require("../../controllers/banner.controller");
 const { getActiveAdvertisements } = require("../../controllers/advertisement.controller");
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.use("/vendor", require("./../vendor/profile.routes"));
 router.use("/product", require("./../product.routes"));
 router.use("/product-purposes", require("./meta.routes"))
 router.use("/rating", require("./rating.routes"))
-router.use("/banner", getAllBanners)
+router.use("/banner", getActiveBanners)
 router.get("/advertisements/active", getActiveAdvertisements)
 router.use("/search", require("./search.routes"))
 router.use("/categories", require("./categories.routes"))
